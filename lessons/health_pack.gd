25dbc028f745c29386352d2b97be744103fedfd0
lpezch <lelopez-chavarria@s.sfusd.edu>
2025-12-11 19:07:47 -0800

    M5L3 Completed

--- lessons/health_pack.gd
+++ lessons/health_pack.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
-
-
-func _on_area_entered(area: Area2D) -> void:
-	queue_free()
-
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(_on_area_entered: Area2D) -> void:
+	queue_free()
